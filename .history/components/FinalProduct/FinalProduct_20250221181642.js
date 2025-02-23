5fa5d06c1fff0ca33fae70069e7e23c0a1539be6
gavinPahal <gpahal4@my.bcit.ca>
2025-02-22 16:38:54 -0800

    new

---
+++ .history/components/FinalProduct/FinalProduct_20250221181642.js
@@ -0,0 +1,16 @@
+import styles from "./FinalProduct.module.css";
+
+const FinalProduct = () => {
+  return (
+    <section className={styles.finalProduct}>
+      <h2 className={styles.title}>Final Product</h2>
+      <a href="https://pocketpals.vercel.app/" class={styles.clickableApp} target="_blank">
+      <img src="/images/iphone-x-mockup 1.png" alt="Pocket Pals Final Product" />
+</a>
+
+      <img src="/images/iphone-x-mockup 1.png" alt="Pocket Pals Final Product" />
+    </section>
+  );
+};
+
+export default FinalProduct;
---
+++ .history/components/MagazineDescription/MagazineDescription_20250222150219.js
@@ -0,0 +1,26 @@
+import styles from "./MagazineDescription.module.css";
+
+const Mockups = () => {
+  return (
+    <section className={styles.mockups}>
+      <div className={styles.imageContainer}>
+        <img src="/images/Rectangle 65 (3).png" alt="Hand holding a Tiger Claw can" />
+      </div>
+      <div className={styles.textContainer}>
+        <h2>Magazine</h2>
+        <p>
+        INSPIRA is a magazine dedicated to Swedish interior design, where minimalism blends with warmth. It’s not
+            merely about simplicity; it’s about crafting spaces that feel both effortless and intentional. Each issue
+            delves into clean aesthetics, functional beauty, and the art of living with less but better. My design
+            philosophy aligns seamlessly with this vision. I emphasize simplicity with purpose, clean layouts, intuitive
+            navigation, and a user-centered approach that makes every interaction smooth. Every decision, from
+            typography to spacing, is deliberate, ensuring clarity without clutter. Good design should feel natural and
+            welcoming, guiding users effortlessly. INSPIRA embodies this philosophy, demonstrating that simplicity is
+            not just a style—it’s a way of life.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Mockups;
